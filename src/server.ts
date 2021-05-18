42cf6ec2b056716ccb93448d62418bae12e6362d
kaaax0815 <999999bst@gmail.com>
2021-05-18 12:10:35 +0200

    ✨ Total Player Count

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,27 @@ import { Client } from 'discord.js';
 import express from 'express';
 import pMS from 'pretty-ms';
 
-function keepAlive(client: Client): void {
+async function keepAlive(client: Client): Promise<void> {
   const server = express();
   // Get Port from Env or Fixed (for Heroku)
   const port = process.env.PORT || 8080;
+  // Get Array of all Guilds
+  const guilds = client.guilds.cache.map((guilds) => guilds);
+  let totalmembers = 0;
+  // Get Member Count (Bots included)
+  for (const guild of guilds) {
+    totalmembers += guild.memberCount;
+  }
   server.all('/', (req, res) => {
-    // Send IFrame of Discord Server
+    // Cool Website
     res.send(
       `<b><img src="${client.user?.avatarURL()}" /><br>${
         client.user?.username
       }</b> is ready!<br>ClientID: <b>${client.user!.id}</b><br>Serving in <b>${
-        client.guilds.cache.size
-      }</b> Servers<br>Running since: <b>${new Date(
+        guilds.length
+      }</b> Servers with a Total of <b>${JSON.stringify(
+        totalmembers
+      )}</b> Members<br>Running since: <b>${new Date(
         Date.now() - client.uptime!
       ).toLocaleString()}</b> - Uptime: <b>${pMS(client.uptime!)}</b>`
     );
