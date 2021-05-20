8e040d8c8f42e39be9b9c4373d2db637d5edec78
kaaax0815 <999999bst@gmail.com>
2021-05-20 16:03:13 +0200

    🏓 ping command

---
+++ src/commands/general/ping.ts
@@ -0,0 +1,24 @@
+import { Client, Message, MessageEmbed } from 'discord.js';
+
+import { config } from '../../index';
+
+module.exports = {
+  info: {
+    name: 'ping',
+    description: 'Pong!',
+    usage: '',
+    aliases: ['latency']
+  },
+  run: async function (client: Client, message: Message) {
+    return message.channel.send('Loading data').then(async (msg) => {
+      msg.deletable ? msg.delete() : null;
+      const ping = new MessageEmbed()
+        .setColor('RANDOM')
+        .setDescription(`Pong! 🏓`)
+        .addField('Latency', msg.createdTimestamp - message.createdTimestamp + 'ms', true)
+        .addField('WebSocket', Math.round(client.ws.ping) + 'ms', true)
+        .setFooter(`Use ${config.prefix}invite to add/invite the Bot to your server`);
+      message.channel.send(ping);
+    });
+  }
+};
