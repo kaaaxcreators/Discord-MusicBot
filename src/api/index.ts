cd2cdb260d79f589a82b90b4b500dacf46ae75bb
kaaax0815 <999999bst@gmail.com>
2021-06-21 19:12:40 +0200

    404 Page

--- src/api/index.ts
+++ src/api/index.ts
@@ -60,4 +60,24 @@ api.use('/', Static(join(__dirname, '../../assets')));
 
 api.use('/', routes);
 
+// 404 Error Handling at the End
+api.all('*', (req, res) => {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.sendFile(join(__dirname, '../../views/404.html'));
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 export default api;
